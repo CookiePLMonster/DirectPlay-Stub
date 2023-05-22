569474062095e57b04825400629a0a94d30aaecb
Silent <zdanio95@gmail.com>
2023-05-22 18:17:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# DirectPlay Stub
+
+An empty stub `dplayx.dll` file designed to remove a dependency on legacy DirectPlay in cases
+where it's not really needed.
---
+++ source/VersionInfo.lua
@@ -0,0 +1,7 @@
+defines { 
+    "rsc_FullName=\"DirectPlay Stub\"",
+    "rsc_MinorVersion=0",
+    "rsc_RevisionID=1",
+    "rsc_BuildID=0",
+    "rsc_Copyright=\"2023\""
+}
